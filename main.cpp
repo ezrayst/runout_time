b7ca59b230678bfabb63b964ea65a9ba5bdd99ab
Ezra Tjung <ezrayst@berkeley.edu>
2017-05-23 23:41:44 -0700

    stable version

--- main.cpp
+++ main.cpp
@@ -19,9 +19,9 @@ int main() {
     //! Reading input file
     //! Error would be reported
 
-
     //! Initialize a vector runout to contain the maximum value in x-dir
     std::vector<double> runout;
+    std::vector<double> time_step;
 
     //! User input inputFilename and outputFilename
     std::string foldername;
@@ -36,47 +36,71 @@ int main() {
     outputfilename = foldername + "runout.txt";
 
     //! Loop through different input file name
-    for (unsigned time_step = 0; time_step <= 20; ++time_step) {
-      if (time_step < 10) {
-        inputfilename = foldername + "Stress0" + std::to_string(time_step) + "000.vtk";
-      }
-      else {
-        inputfilename = foldername + "Stress" + std::to_string(time_step) + "000.vtk";
-      }
-       
+    for (unsigned t = 0; t <= 20; ++t) {
 
-      std::cout << inputfilename << "\n";
+      //! Get vector time_step that contains the time index
+      time_step.push_back(t * 1000);
+
+      //! Get inputfilename
+      if (t < 10) {
+        inputfilename = foldername + "Stress0" + std::to_string(t) + "000.vtk";
+      } else {
+        inputfilename = foldername + "Stress" + std::to_string(t) + "000.vtk";
+      }
 
       //! Open input file and store x-dir
       std::ifstream inputFile;
       inputFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
       inputFile.open(inputfilename);
 
-      // unsigned total_num_points;
-      // std::vector<double> ycoord;
-      // double value;
+      //! Declare temporary variable
+      char unused;
+      double value;
+      unsigned total_num_points;
+      std::vector<double> xcoord;
 
-      // inputFile >> total_num_points;
+      //! Loop through the unused characters
+      for (unsigned i = 0; i < 66; ++i) {
+        inputFile >> unused;
+      }
 
-      // std::cout << total_num_points;
+      //! Get total number of points
+      inputFile >> total_num_points;
+
+      //! Again loop through the unused characters
+      for (unsigned i = 0; i < 5; ++i) {
+        inputFile >> unused;
+      }
+
+      //! Loop through points to get all x coords
+      for (unsigned i = 0; i < total_num_points; ++i) {
+        inputFile >> value;
+        xcoord.push_back(value);
+        inputFile >> value;
+        inputFile >> value;
+      }
 
       inputFile.close();
-      std::cout << "The input file for time step: " 
-                << std::to_string(time_step)
+      std::cout << "The input file for time step: " << std::to_string(t)
                 << "000 has been read."
                 << "\n";
+
+      //! Get maximum value of runout
+      runout.emplace_back(*std::max_element(xcoord.begin(), xcoord.end()));
+
+      std::cout << runout.back() << "\n";
     }
-    
+
     //! Open output file and store all data
-    // std::ofstream outputFile(outputfilename);
+    std::ofstream outputFile(outputfilename);
 
-    // outputFile << total_num_points << "\n";
-    // for (unsigned i = 0; i < total_num_points; ++i) {
-    //       }
+    for (unsigned i = 0; i <= 20; ++i) {
+      outputFile << time_step.at(i) << "\t" << runout.at(i) << "\n";
+    }
 
-    // outputFile.close();
-    // std::cout << "The output file has been generated."
-    //           << "\n";
+    outputFile.close();
+    std::cout << "The output file has been generated."
+              << "\n";
 
   }
 
